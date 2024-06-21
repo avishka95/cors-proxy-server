8efb98197cf51bf4eac134f14d577cdccc77e805
Avishka Ariyaratne <avishka95@gmail.com>
2024-06-21 15:52:15 +0530

    Change main file name

--- express-server/server.js
+++ express-server/server.js
@@ -1,19 +1,21 @@
 const express = require('express');
+require('dotenv').config();
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+const target = process.env.TARGET || 'http://localhost:9090';
 
 // Use CORS middleware
 app.use(cors());
-
 // Create a proxy route
 app.use('/', createProxyMiddleware({
-    target: process.env.TARGET || 'http://localhost:9090',
+    target,
     changeOrigin: true,
   }));
 
 // Start the server
 app.listen(port, () => {
   console.log(`CORS Proxy Server is running on http://localhost:${port}`);
+  console.log(`CORS Proxy Server is forwarding requests to ${target}`);
 });
